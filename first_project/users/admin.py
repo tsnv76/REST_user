ef71747b24d6dcba4b1d8f29b05c8d05f492f381
tsnv76 <tsnv76@yandex.ru>
2021-12-09 21:58:17 +0700

    add files homework for lesson1

--- first_project/users/admin.py
+++ first_project/users/admin.py
@@ -8,16 +8,17 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active',)
-    list_filter = ('email', 'is_staff', 'is_active',)
+    list_display = ('email', 'last_name', 'first_name', 'date_joined', 'is_staff', 'is_active',)
+    list_filter = ('email', 'last_name', 'first_name', 'date_joined', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password', 'last_name', 'first_name', 'date_joined')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', \
+                       'last_name', 'first_name', 'date_joined', 'is_staff', 'is_active')}
         ),
     )
     search_fields = ('email',)
--- first_project/users/forms.py
+++ first_project/users/forms.py
@@ -5,10 +5,10 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('email',)
+        fields = ('email', 'last_name' , 'first_name', 'date_joined')
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = ('email',)
+        fields = ('email', 'last_name', 'first_name', 'date_joined')
