73a98b1232d0ab9314f3ce467c26c24a8eb2698a
tsnv76 <tsnv76@yandex.ru>
2022-01-04 16:41:12 +0700

    add files homework for lesson 8

--- first_project/todoapp/tests.py
+++ first_project/todoapp/tests.py
@@ -1,30 +1,51 @@
 from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, force_authenticate
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APITestCase
+from mixer.backend.django import mixer
 
-from django.contrib.auth.models import User
-
+from todoapp.models import ToDo
+from todoapp.views import ProjectModelViewSet
 from users.models import CustomUser
 from users.views import UserModelViewSet
 
 
-class TestProjectView(TestCase):
+class TestUsersView(TestCase):
 
-    def test_get_list(self):
-        admin = CustomUser.objects.create_superuser(email='test@test.ru', password='123')
+    def setUp(self) -> None:
+        self.admin = CustomUser.objects.create_superuser(email='test@test.ru', password='123')
+
+    def test_get_list_user(self):
         factory = APIRequestFactory()
-        request = factory.post('/api/project/', {
-            'first_name': 'Александр',
-            'last_name': 'Пушкин',
-            'birthday_year': 1799
-        })
-        force_authenticate(request, user=admin)
-        view = UserModelViewSet.as_view({'post': 'create'})
-        response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['last_name'], 'Пушкин')
-        request = factory.get('/api/authors/')
-        force_authenticate(request, user=admin)
+        request = factory.get('/api/users/')
+        force_authenticate(request, user=self.admin)
         view = UserModelViewSet.as_view({'get': 'list'})
         response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestProjectView(APITestCase):
+
+    def setUp(self) -> None:
+        self.admin = CustomUser.objects.create_superuser(email='test@test.ru', password='123')
+
+    def test_get_list(self):
+        factory = APIRequestFactory()
+
+        request = factory.post('/api/project/', {
+            "id": 1,
+            "name": "First",
+            "repo_link": "http://www.ru",
+            "users": [
+                1
+            ]
+         })
+        force_authenticate(request, user=self.admin)
+        view = ProjectModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_client_todo(self):
+        todo = mixer.blend(ToDo)
+        self.client.login(username='test@test.ru', password='123')
+        response = self.client.get('/api/todo/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
