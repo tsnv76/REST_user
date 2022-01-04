e6973940cd60c13ebe94f3ebb253ce3c8fb90a63
tsnv76 <tsnv76@yandex.ru>
2022-01-04 15:16:08 +0700

    add files homework for lesson 8

--- first_project/todoapp/tests.py
+++ first_project/todoapp/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+from users.models import CustomUser
+from users.views import UserModelViewSet
+
+
+class TestProjectView(TestCase):
+
+    def test_get_list(self):
+        admin = CustomUser.objects.create_superuser(email='test@test.ru', password='123')
+        factory = APIRequestFactory()
+        request = factory.post('/api/project/', {
+            'first_name': 'Александр',
+            'last_name': 'Пушкин',
+            'birthday_year': 1799
+        })
+        force_authenticate(request, user=admin)
+        view = UserModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['last_name'], 'Пушкин')
+        request = factory.get('/api/authors/')
+        force_authenticate(request, user=admin)
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
